ae92b44ad2a5737791b030432bcbbf82b0e9eab4
Ronan <bayrule@hotmail.com>
2016-03-28 09:12:34 +0100

    I got 99 problems but commitment issues aren't one

--- FYP/Quotr/www/js/controllers/controller.js
+++ FYP/Quotr/www/js/controllers/controller.js
@@ -1,82 +1,17 @@
-Quotr.controller('Controller',['$scope','Home','Result','History','Top20',function($scope,Home,Result,History,Top20) {
+Quotr.controller('Controller',['$scope','Home',function($scope,Home) {
   
-	var uuid="";
-
-	document.addEventListener("deviceready", onDeviceReady, false);
-	function onDeviceReady() {
-    	uuid=device.uuid;
-	}
-
-	$scope.record = function() {
-
-
-
-		bootbox.dialog({
-                title: "Edit Your Quote",
-                message: '<div class="row"width="100%">  ' +
-                    '<div class="col-md-12"width="100%"> ' +
-                    '<form class="form-horizontal"> ' +
-                    '<div class="form-group" width="100%"> ' +
-                    '<div class="col-md-4" width="100%" > ' +
-                    '<textarea id="quoteText"  type="text" width="300px" style="font-size: 50px;" rows="12"></textarea> ' +
-                    '<span class="help-block"></span> </div> ' +
-                    '</div> ' +
-                    '</form> </div> </div>',
-                buttons: {
-					cancel:{
-						label:"Cancel",
-						callback: function(){
-
-						}
-					},
-                    success: {
-                        label: "Send",
-                        className: "btn-success",
-						size: "large",
-                        callback: function () {
-                            Result.search(uuid,$("#quoteText").val());
-                        }
-                    }
-
-                }
-            }
-        );
-
-		Home.record();
-
+	var record=false;
+  
+	$scope.search = function() {
+	  
+		if(record==false){
+			record=true;
+		}
+		else{
+			record=false;
+		}
+		Home.search(record);
 	};
-
-	$scope.search= function() {
-		//Send to contents of text box to result.js service where user will be brought to result page
-		Result.search(uuid,$("#quoteText").val());
-	};
-	$scope.cancel= function() {
-		//Cancels search
-		Result.cancel();
-	};
-
-
-	$scope.recommend= function() {
-
-		Result.recommend($('#poster').attr('alt'),uuid);
-	};
-
-	$scope.showhistory= function() {
-		//Show history of a user
-
-		History.showhistory(uuid);
-	};
-
-	$scope.top20= function(type) {
-		//Top 20 searched Series and Movies
-		Top20.top20(type);
-	};
-
-	$scope.retrieve= function(imdbId) {
-		Result.search(uuid,imdbId);
-	};
-
-
 
 
 }]);
--- FYP/Quotr/www/js/services/home.js
+++ FYP/Quotr/www/js/services/home.js
@@ -1,24 +1,21 @@
 Quotr.factory('Home', ['$rootScope',function($scope) {
-
-    //$("#searchText").hide();
-
+  
+  
 	return{
-
-		record: function(){
-
-			var maxMatches = 1;
-			var promptString = "Speak now"; // optional
-			var language = "en-US";                     // optional
-			window.plugins.speechrecognizer.startRecognize(function(result){
-				$("#quoteText").val(result);
-
-			}, function(errorMessage){
-
-				console.log("Error message: " + errorMessage);
-			}, maxMatches, promptString, language);
-			//location.href = "#/search";
+		
+		search: function(record){
+			if(record==true){
+				
+				
+				
+			}
+			else{
+				
+				
+			}
 
 		}
 	};
-
+  
 }]);
+  
