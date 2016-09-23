044e5502af5134f1a70c540031d557b024222c2b
Ronan <bayrule@hotmail.com>
2016-09-23 15:21:02 +0100

    Final Commit
    Excess files deleted.
    Files organized for server side and client side code.

--- Quotr/www/controllers/controller.js
+++ Quotr/www/controllers/controller.js
@@ -1,3 +1,82 @@
-Quotr.controller('Controller',function() {
-	
-});
+Quotr.controller('Controller',['$scope','Home','Result','History','Top20',function($scope,Home,Result,History,Top20) {
+
+	var uuid="";
+
+	document.addEventListener("deviceready", onDeviceReady, false);
+	function onDeviceReady() {
+    	uuid=device.uuid;
+	}
+
+	$scope.record = function() {
+
+
+
+		bootbox.dialog({
+                title: "Edit Your Quote",
+                message: '<div class="row"width="100%">  ' +
+                    '<div class="col-md-12"width="100%"> ' +
+                    '<form class="form-horizontal"> ' +
+                    '<div class="form-group" width="100%"> ' +
+                    '<div class="col-md-4" width="100%" > ' +
+                    '<textarea id="quoteText"  type="text"  style="font-size: 26px;" rows="8"></textarea> ' +
+                    '<span class="help-block"></span> </div> ' +
+                    '</div> ' +
+                    '</form> </div> </div>',
+                buttons: {
+					cancel:{
+						label:"Cancel",
+						callback: function(){
+
+						}
+					},
+                    success: {
+                        label: "Send",
+                        className: "btn-success",
+						size: "large",
+                        callback: function () {
+                            Result.search(uuid,$("#quoteText").val());
+                        }
+                    }
+
+                }
+            }
+        );
+
+		Home.record();
+
+	};
+
+	$scope.search= function() {
+		//Send to contents of text box to result.js service where user will be brought to result page
+		Result.search(uuid,$("#quoteText").val());
+	};
+	$scope.cancel= function() {
+		//Cancels search
+		Result.cancel();
+	};
+
+
+	$scope.recommend= function() {
+
+		Result.recommend($('#poster').attr('alt'),uuid);
+	};
+
+	$scope.showhistory= function() {
+		//Show history of a user
+
+		History.showhistory(uuid);
+	};
+
+	$scope.top20= function(type) {
+		//Top 20 searched Series and Movies
+		Top20.top20(type);
+	};
+
+	$scope.retrieve= function(imdbId) {
+		Result.search(uuid,imdbId);
+	};
+
+
+
+
+}]);
--- flask/testytest.py
+++
@@ -1,4 +0,0 @@
-from googlesearch import GoogleSearch
-gs = GoogleSearch.top_results()
-for url in gs.top_urls():
-  print url
